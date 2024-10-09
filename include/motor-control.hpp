d789675ec02e47e1078d5222bfc6b31bf8ad051d
butterfalls <1731860884@qq.com>
2024-10-09 23:18:04 +0800

    增加start使能所有步进

--- include/motor-control.hpp
+++ include/motor-control.hpp
@@ -2,7 +2,7 @@
  * @Author: butterfalls 1731860884@qq.com
  * @Date: 2024-10-09 17:48:24
  * @LastEditors: butterfalls 1731860884@qq.com
- * @LastEditTime: 2024-10-09 22:36:13
+ * @LastEditTime: 2024-10-09 22:54:59
  * @FilePath: \hw1e:\Files\code\2025ASABE-selection\include\motor-control.hpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -101,4 +101,11 @@ void revolve_ccw(int times){
   digitalWrite(directionPinlb ,LOW);
   digitalWrite(directionPinrb ,LOW);
   pulse_send(times);
+}
+
+void start(){
+  digitalWrite(ENlf ,HIGH);
+  digitalWrite(ENlb ,HIGH);
+  digitalWrite(ENrf ,HIGH);
+  digitalWrite(ENrb ,HIGH);
 }
