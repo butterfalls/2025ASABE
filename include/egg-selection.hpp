085bfbf51905a9f81986bc04d51339842ed2da4b
xxfbdll <wqf19120@163.com>
2024-10-11 17:22:00 +0800

    zengjiazhushi

--- include/egg-selection.hpp
+++ include/egg-selection.hpp
@@ -6,6 +6,7 @@
  * @FilePath: \hw1e:\Files\code\2025ASABE-selection\include\egg-selection.hpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+//yhk test
 #include <Arduino.h>
 #include <stdio.h>
 #include <SoftwareSerial.h>
